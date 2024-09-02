10049de6b7608039993428741722aa8261c0c45f
Syamlal CM <syamjayaraj@gmail.com>
2024-09-02 13:36:10 +0530

    import-export added

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -6,6 +6,7 @@ import strapiCloud from "@strapi/plugin-cloud/strapi-admin";
 import graphql from "@strapi/plugin-graphql/strapi-admin";
 import i18N from "@strapi/plugin-i18n/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
+import importExportEntries from "strapi-plugin-import-export-entries/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
 import customisations from "../../src/admin/app.tsx";
@@ -17,5 +18,6 @@ renderAdmin(document.getElementById("strapi"), {
     graphql: graphql,
     i18n: i18N,
     "users-permissions": usersPermissions,
+    "import-export-entries": importExportEntries,
   },
 });
